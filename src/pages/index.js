cc8b72cdf6a6fa237db3b2d2ff0e94a56c0ac7ab
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-09-11 20:58:26 -0300

    project start, index clean

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Do It</title>
+        <meta name="description" content="your task list, just do it!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
